f991b32b8a69883bae4b9f2df3f33c9a796feea3
Johyn Papin <johynpapin@protonmail.com>
2018-04-24 12:21:44 +0900

    marche pas \o/

--- commands.go
+++ commands.go
@@ -9,11 +9,15 @@ import (
 var commands = make(map[string]command)
 
 func parseCommand(s *discordgo.Session, m *discordgo.MessageCreate, args []string) error {
+	if len(args) == 0 {
+		s.ChannelMessageSend(m.ChannelID, "Que dois-je faire ? Pour avoir la liste des commandes, utilisez la commande `help`.")
+	}
+
 	if command, exist := commands[strings.ToLower(args[0])]; exist {
 		command.Exec(s, m, args[1:])
 	}
 
-	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Je n’ai pas compris la commande `%s`, désolé.", args[0]))
+	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Je n’ai pas compris la commande `%s`, désolé. Pour avoir la liste des commandes, utilisez la commande `help`.", args[0]))
 
 	return nil
 }
