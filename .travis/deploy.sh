4a98ca6f330382f23e2c811a7b8d6eec9e9367a9
Johyn Papin <johynpapin@protonmail.com>
2018-04-27 17:17:32 +0900

    ?

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+go build $TRAVIS_BUILD_DIR
 ssh johynpapin@78.248.188.78 'systemctl --user stop narcisse'
 scp $TRAVIS_BUILD_DIR/narcisse johynpapin@78.248.188.78:/home/johynpapin/narcisse
 ssh johynpapin@78.248.188.78 'systemctl --user start narcisse'
