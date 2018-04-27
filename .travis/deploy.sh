55a610616cbe9e10c91125cee6bdc194678645d5
Johyn Papin <johynpapin@protonmail.com>
2018-04-27 17:19:16 +0900

    mieux

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-go build $TRAVIS_BUILD_DIR
+go install $TRAVIS_BUILD_DIR
 ssh johynpapin@78.248.188.78 'systemctl --user stop narcisse'
-scp $TRAVIS_BUILD_DIR/narcisse johynpapin@78.248.188.78:/home/johynpapin/narcisse
+scp $GOPATH/bin/narcisse johynpapin@78.248.188.78:/home/johynpapin/narcisse
 ssh johynpapin@78.248.188.78 'systemctl --user start narcisse'
