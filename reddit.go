ae942c1dd7833ab29e8103c9cd376da9693499ef
Johyn Papin <johynpapin@protonmail.com>
2018-04-28 23:28:30 +0900

    debug

--- reddit.go
+++ reddit.go
@@ -3,10 +3,11 @@ package main
 import (
 	"github.com/turnage/graw/reddit"
 	"fmt"
+	"github.com/spf13/viper"
 )
 
 func connectReddit() (reddit.Bot, error){
-	bot, err := reddit.NewBotFromAgentFile("reminderbot.agent", 0)
+	bot, err := reddit.NewBotFromAgentFile(viper.GetString("reddit.agent_file"), 0)
 	if err != nil {
 		return nil, fmt.Errorf("error creating Reddit session: %s", err)
 	}
